77212b17d6a4be59bd11d20556f776ddf36ac2c2
Ali Hassan <ali.software.dev@gmail.com>
2022-06-16 20:58:57 +0100

    Added project

---
+++ JDTechTask/JDTechTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JDTechTask
+//
+//  Created by Ali Hassan on 16/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
